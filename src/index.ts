7bfe7dbfe4464a06c6847f444495e2d56fdb7988
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2024-11-07 19:49:02 -0600

    Add opengl

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("v");
+class WebGLApp {
+  private canvas: HTMLCanvasElement;
+  private gl: WebGL2RenderingContext;
+
+  constructor() {
+    this.canvas = document.querySelector("#glCanvas") as HTMLCanvasElement;
+    this.gl = this.canvas.getContext("webgl2") as WebGL2RenderingContext;
+
+    if (!this.gl) {
+      throw new Error("WebGL2 not supported");
+    }
+
+    this.init();
+  }
+
+  private init(): void {
+    // Set clear color to black
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+  }
+}
+
+new WebGLApp();
